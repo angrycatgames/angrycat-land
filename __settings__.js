27590277df932d8063286199128a562242fe19e7
angrycatgames <118334365+angrycatgames@users.noreply.github.com>
2022-12-22 13:11:10 +1100

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1613787.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 114799281, 114810166, 114819687, 114833868, 115194986, 115196748, 115521548, 116052489, 116052490, 116052492 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/114807088/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/114807089/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/114807087/1/ammo.js', 'preload' : true},
+];
